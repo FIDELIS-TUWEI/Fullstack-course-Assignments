a3fc8781c8f9680a96a4ad442ccb0118d48ec6bb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-14 04:59:53 +0300

    Backend error middleware logic added, crud logic added

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-// Unknown endpoints
-const notFound = (request, response) => {
-    response.status(404).send({ error: "Unknown Endpoint!" })
-}
-
 app.get("/", (request, response) => {
     return response.send("Backend Server is running")
 })
@@ -27,13 +22,6 @@ app.get("/", (request, response) => {
 app.get("/api/persons", (request, response) => {
     Phonebook.find({}).then(persons => {
         response.json(persons)
-    });
-});
-
-// get request for a single resource
-app.get("/api/persons/:id", (request, response) => {
-    Phonebook.findyId(request.params.id).then(person => {
-        response.json(person)
     });
 });
 
@@ -57,10 +45,40 @@ app.post("/api/persons", (request, response) => {
     });
 });
 
-// delete request
-app.delete("/api/persons/:id", (request, response) => {
+// get request for a single resource
+app.get("/api/persons/:id", (request, response, next) => {
+    Phonebook.findyId(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        }).catch(error => next(error));
+});
 
-    response.status(204).end();
+// put request for existing resource
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number
+    };
+
+    Phonebook.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        }).catch(error => next(error));
+})
+
+// delete request
+app.delete("/api/persons/:id", (request, response, next) => {
+    Phonebook.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 });
 
 app.get("/info", (request, response) => {
@@ -73,7 +91,26 @@ app.get("/info", (request, response) => {
     )
 });
 
-app.use(notFound)
+// Unknown endpoints
+const notFound = (request, response) => {
+    response.status(404).send({ error: "Unknown Endpoint!" })
+};
+
+app.use(notFound);
+
+// Error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        response.status(400).send({ error: "Malformatted ID" })
+    }
+
+    next(error)
+};
+
+app.use(errorHandler);
+
 // Disable server fingerprinting
 app.disable('x-powered-by');
 
