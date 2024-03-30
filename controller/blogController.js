304bf16ff3ea6876897122b5560981213ef6d4d2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-30 10:41:55 +0300

    post/get request controller logic

--- controller/blogController.js
+++ controller/blogController.js
@@ -5,4 +5,23 @@ blogsRouter.get('/', (request, response) => {
     response.send("Blog List Server running")
 });
 
+// request to fetch all blogs
+blogsRouter.get("/api/v1/blogs", (request, response) => {
+    Blog.find({})
+        .then(blogs => {
+            response.json(blogs);
+        })
+})
+
+// request to create new blog
+blogsRouter.post("/api/v1/blogs", (request, response, next) => {
+    const blog = new Blog(request.body);
+
+    blog.save()
+        .then(result => {
+            response.status(200).json(result)
+        })
+        .catch(error => next(error));
+});
+
 module.exports = blogsRouter;
