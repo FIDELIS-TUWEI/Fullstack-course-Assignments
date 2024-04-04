2b7a1e1d610b34f120c742643a79a6707a070bd0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-04 07:14:17 +0300

    favorite blog test helper function logic

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,9 +4,22 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     return blogs.reduce((total, blogs) => total + blogs.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+
+    const favorite = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current));
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
