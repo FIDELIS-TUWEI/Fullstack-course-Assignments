2704248e8ed47a94b07ca6393625e158c83daf0c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-04 08:31:46 +0300

    test case for favorite blog to be deep strict equal

--- tests/list.test.js
+++ tests/list.test.js
@@ -97,4 +97,24 @@ describe('favorite blog', () => {
         const result = listHelper.favoriteBlog(emptyList);
         assert.strictEqual(result, null);
     });
+
+    test('when list has only one blog equals the likes of that', () => {
+        const listWithOneBlog = [
+            {
+              _id: '5a422aa71b54a676234d17f8',
+              title: 'Go To Statement Considered Harmful',
+              author: 'Edsger W. Dijkstra',
+              url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+              likes: 5,
+              __v: 0
+            }
+        ];
+
+        const result = listHelper.favoriteBlog(listWithOneBlog);
+        assert.deepStrictEqual(result, {
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            likes: 5 
+        });
+    })
 })
