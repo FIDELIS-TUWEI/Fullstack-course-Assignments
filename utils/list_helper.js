093f03deaf636fae71d9c413167461bd04dd0a59
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-04 08:57:27 +0300

    function for most blogs test case

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -20,8 +20,31 @@ const favoriteBlog = (blogs) => {
     return favoriteBlog;
 };
 
+const mostBlogs = (blogs) => {
+    const authorCounts = blogs.reduce((count, blog) => {
+        count[blog.author] = (count[blog.author] || 0) + 1;
+        return count;
+    }, {});
+
+    let mostBlogsAuthor = '';
+    let maxBlogs = 0;
+
+    for (const author in authorCounts) {
+        if (authorCounts[author] > maxBlogs) {
+            mostBlogsAuthor = author;
+            maxBlogs = authorCounts[author];
+        }
+    }
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: maxBlogs
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 };
