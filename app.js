78dbf791e6d65cb1b90579f85ee373a97a18bc65
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-08 11:01:58 +0300

    project cleanup

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
-const config = require("./utils/config");
+//const config = require('./utils/config');
 const express = require("express");
-const mongoose = require("mongoose");
+//const mongoose = require('mongoose');
 const cors = require("cors");
 const app = express();
-const blogsRouter = require("./controller/blogController");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
+//const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+//const logger = require('./utils/logger');
 
-mongoose.set("strictQuery", false);
-
-logger.info("Connecting to", config.MONGODB_URI);
-
-// Database connection
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info("Connected to MongoDB Database")
-    })
-    .catch((error) => {
-        logger.error("Error connecting to MongoDB", error.message)
-    });
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
 
 app.use(express.json());
 app.use(cors());
-app.use(middleware.requestLogger);
+app.use(middleware.requestLogger)
 app.disable("x-powered-by");
 
-app.use('/api/v1/blogs', blogsRouter);
-app.use(middleware.unKnownEndpoint);
+app.get('/', (request, response) => {
+    response.send('Backend Server running...')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 module.exports = app;
