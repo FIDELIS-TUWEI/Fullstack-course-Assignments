4d8cd9daeca602b49cc953867ace278ed69ec6cb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-08 20:50:41 +0300

    mongodb database connection added

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 const Person = require("./models/person");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 };
+
+mongoose.set("strictQuery", false);
+
+logger.info("Connecting to", config.MONGODB_URI);
+
+// Database connection
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("Connected to MongoDB Database")
+    })
+    .catch((error) => {
+        logger.error("Error connecting to MongoDB", error.message)
+    });
   
 
 app.use(express.json());
