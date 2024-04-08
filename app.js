13a85cd36fa250517a69e2f6ac526c63d98475b3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-08 11:32:43 +0300

    route to delete person

--- app.js
+++ app.js
@@ -49,6 +49,14 @@ app.get('/api/persons/:id', (request, response) => {
     }
 });
 
+// deleting a resource route
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end();
+})
+
 app.get('/info', (request, response) => {
     const timeReceived = new Date();
     const entriesCount = persons.length;
