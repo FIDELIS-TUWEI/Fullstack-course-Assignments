a8d00879f5d46e295c090195ae68653b61f804f2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-09 08:28:47 +0300

    error handler message errors modified to be displayed in frontend

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -6,7 +6,8 @@ const errorHandler = (error, response, request, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        const errors = Object.values(error.errors).map(err => err.message)
+        return response.status(400).json({ errors });
     }
 
     next(error);
