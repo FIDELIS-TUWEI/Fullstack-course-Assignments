a271034fd3802b38a10491aeb0a139d6049a4c05
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-30 05:30:33 +0300

    blogs routes modified

--- controller/blogController.js
+++ controller/blogController.js
@@ -6,13 +6,13 @@ blogsRouter.get('/', (request, response) => {
 });
 
 // request to fetch all blogs
-blogsRouter.get("/api/v1/blogs", async (request, response) => {
+blogsRouter.get("/blogs", async (request, response) => {
     const blogs = await Blog.find({});
     response.json(blogs);
 })
 
 // request to create new blog
-blogsRouter.post("/api/v1/blogs", async (request, response, next) => {
+blogsRouter.post("/blogs", async (request, response, next) => {
     const blog = new Blog(request.body);
 
     const savedBlog = await blog.save();
