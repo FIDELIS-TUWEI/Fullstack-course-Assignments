a8cffdecf3c9ee2ffe1936dbdd0456f4ab7a2f86
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-08 05:07:56 +0300

    logic to fetch single resource with id

--- controller/blogController.js
+++ controller/blogController.js
@@ -19,4 +19,13 @@ blogsRouter.post("/blogs", async (request, response, next) => {
     response.status(201).json(savedBlog)
 });
 
+// request to fetch single source with id
+blogsRouter.get("/blog/:id", async (request, response) => {
+    const blog = await Blog.findById(request.params.id);
+
+    if (!blog) return res.status(400).json({ error: "Blog not found" });
+
+    res.status(200).json(blog);
+})
+
 module.exports = blogsRouter;
