6513c2b299b02cba16e8c7c94c77f295286d34b0
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-08 05:08:33 +0300

    test case to return object with id property

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,12 +5,27 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app);
 
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
+
 test.only('blogs are returned as json', async () => {
     await
         api
         .get('/api/v1/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+});
+
+
+test('should return object with id property', async () => {
+    const blog = await helper.blogsInDb();
+
+    const resultBlog = await api
+        .get(`/api/v1/blogs/${blog.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/).toHaveProperty('id')
+
+    assert.deepStrictEqual(resultBlog.body, blog);
 });
 
 after(async () => {
