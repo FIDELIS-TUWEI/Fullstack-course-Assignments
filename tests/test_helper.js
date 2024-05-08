ec34178569aac14a3f887c4f873cb751d3867fb0
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-08 05:08:51 +0300

    tes helper file created

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -2,9 +2,34 @@ const Blog = require("../models/blog");
 
 const initialBlogs = [
     {
+        _id: '65ed1532b6eb2b4391edb017',
         title: "Psychology of Money",
         author: "Morgan Housel",
         url: "https://psychologyofmoney.org",
         likes: 100
+    },
+    {
+        _id: '65ed1532b6eb2b4391edb027',
+        title: "Rationl Male",
+        author: "Rollo Tomassi",
+        url: "https://rationalmale.info",
+        likes: 105
     }
 ];
+
+const nonExistingId = async () => {
+    const blog = new Blog({ title: '48 laws of power' })
+    await blog.save()
+    await blog.deleteOne();
+
+    return blog._id.toString();
+};
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({});
+    return blogs.map(blog => blog.toJSON());
+};
+
+module.exports = {
+    initialBlogs, nonExistingId, blogsInDb
+};
