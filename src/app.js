fe783bb57ba6f9a16d8a58fa0ebd31486587a99b
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-16 08:40:59 +0300

    additional parse middleware added

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ mongoose.connect(config.MONGODB_URI)
         logger.error("Error connecting to MongoDB", error.message)
     });
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cors());
 app.use(middleware.requestLogger);
 app.disable("x-powered-by");
