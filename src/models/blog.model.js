c1dfb5baff0f1884e53019a1a452b9fd31e15044
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-16 09:30:43 +0300

    added author ref to user model

--- src/models/blog.model.js
+++ src/models/blog.model.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
     title: String,
-    author: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     url: String,
     likes: Number,
 });
